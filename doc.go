627ca0be1bec0ab8981a669c92559a02c15b036f
Kazuhiro Yamamoto <yamamotokzj@intellilink.co.jp>
2019-04-10 17:03:59 +0900

    Update comment

--- doc.go
+++ doc.go
@@ -5,11 +5,20 @@
 //   ------- | ------ | ----- | ----- | ----- | ---- | ------ | --------
 //   abc     | OK     | 302   | 1.234 | true  | 7890 | abc\nd | あいうえお
 //           | NG     | -0x20 | -5    | F     | 3333 | \\n\|  | 日本語
+//   def     | OK     |       | 5.67  |       | 210  | \\     | いろは  \
+//   ghi     |        | 404   |       | false |      | \|     | にほへ
 //
-// A row filled with '-' is assumed as delimiter.
-// Header is rows above the first delimiter and body is below that delimiter.
-// Delimiters in body are ignored. Empty lines above header are ignored.
-// Table ends with an empty line. Following lines are ignored.
+// A row filled with '-' is a delimiter. It is ignored.
+// First row is header. Following rows are body.
+//
+// Empty lines and lines filled with white spaces above header are ignored.
+// Table ends with an empty line or a line filled with white spaces.
+// Once table ends, following lines are ignored.
+//
 // Escape sequences can be used in values. Those are "\n" (unescaped into LF),
 // "\\" (\), and "\|" (|).
+//
+// A row ends with "\" indicates it continues to the next row.
+// In above example 5th row and 6th row are merged when unmarshaling.
+// So the value of "string" column is "def ghi".
 package table
